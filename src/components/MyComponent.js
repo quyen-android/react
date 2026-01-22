f91101435e71ed2d15afaec0904f49fe416c03ef
quyen-android <vanquyenk22ute@gmail.com>
2026-01-22 09:30:46 +0700

    video 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -12,11 +12,20 @@ class MyComponent extends React.Component{
         age: 22
     };
 
+    handleClick (event){
+        console.log(">>>click me my button"); 
+        console.log("My name is", this.state.name); 
+    }
+    handleOnMouseOver(event){
+        console.log(event.pageX)
+    }
     render(){
         return (
             <div>
 
                 My name is {this.state.name}
+                <button onClick={this.handleClick}>click</button>
+                <button onMouseOver = {(event) => {this.handleOnMouseOver(event)}}>move</button>
             </div>
         );
     }
