a26d1f8828895e7ed71f35e1ddf2f1bbda5192e4
quyen-android <vanquyenk22ute@gmail.com>
2026-01-24 21:58:16 +0700

    image

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './DisplayInfor.scss';
+import logo from '../logo.svg';
 
 class DisplayInfor extends React.Component{
     // Destructuring array
@@ -17,6 +18,7 @@ class DisplayInfor extends React.Component{
         const {listUsers} = this.props;
         return(
             <div className="display-infor-container">
+                <img src={logo}></img>
                 <div>
                     <span onClick={(event) => { this.handleShowHide()}}>
                         {this.state.isShowListUser === true? "Hide list user: " : "Show list user: "} 
