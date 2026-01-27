d30f6721bc62de79f02a6ff78a5e7db70dcb7899
quyen-android <vanquyenk22ute@gmail.com>
2026-01-27 22:45:02 +0700

    Design homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,9 +18,9 @@ const Header = () => {
           </Nav>
           <Nav>
              <NavDropdown title="Setting" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Log out</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+                <NavDropdown.Item >Log in</NavDropdown.Item>
+                <NavDropdown.Item >Log out</NavDropdown.Item>
+                <NavDropdown.Item >Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,12 @@
+import videoHomePage from '../../assets/video-homepage.mp4'
 const HomePage = (props) =>{
     return(
         <div className="homepage-container">
-            Home page components
+            <video autoPlay muted loop >
+                <source 
+                    type="video/mp4" 
+                    src={videoHomePage}/>
+            </video>
         </div>
     )     
 }
