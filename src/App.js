2b97d309f9ef8d632ce902a85612da3ae46b48a2
quyen-android <vanquyenk22ute@gmail.com>
2026-01-27 16:18:20 +0700

    Dieu huong tran with links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 
+import { Link } from 'react-router-dom';
 import './App.scss';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 
 const App = () =>{
 
 
   return(
     <div className='app-container'>
-        <Header/>
+      <Header/>
+      <div> 
+        test links
+        <button>
+          <Link to='/admins'>
+            Go to the admins page
+          </Link>
+        </button>
+        <button>
+          <Link to='/users'>
+            Go to the user page
+          </Link>
+        </button>
+      </div>
     </div>
+
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,27 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { 
+    BrowserRouter,
+    Routes,
+    Route
+  } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import User from './components/User/User';
+import Admin from './components/Admin/Admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App/>}></Route>
+          <Route path='users' element={<User/>}></Route>
+          <Route path='admins' element={<Admin/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      
   </Provider>
 );
 
