7c4efbc466717ad75d527d1d60ee984a7191d89b
quyen-android <vanquyenk22ute@gmail.com>
2026-01-28 09:30:14 +0700

    Design new header

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -7,6 +7,15 @@ const HomePage = (props) =>{
                     type="video/mp4" 
                     src={videoHomePage}/>
             </video>
+            <div className='homepage-content'>
+                <div className='title-1'>Get 3.5x more data with a form expert</div>
+                <div className='title-2'>Backed by over a decade of experience, Typeform 
+                    AI helps you build expertly-designed, best-
+                    practice forms proven to get more responses.</div>
+                <div >
+                    <button className='title-3'>Get's started. It's free</button>
+                </div>
+            </div>
         </div>
     )     
 }
