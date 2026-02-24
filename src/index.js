04b25b0ce0b5be80a671b19bc9753d710d706f02
quyen-android <vanquyenk22ute@gmail.com>
2026-02-24 16:33:35 +0700

    Redux-persist

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import {store,persistor} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 import { 
     BrowserRouter,
     Routes,
@@ -17,9 +18,11 @@ import 'nprogress/nprogress.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <Layout/>
       </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
 
